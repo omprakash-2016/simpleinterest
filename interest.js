a76d34a9cd108c621c0bb1a466b2878cab8892dd
omprakash-2016 <77918595+omprakash-2016@users.noreply.github.com>
2021-04-19 17:52:32 +0530

    Add files via upload

---
+++ interest.js
@@ -0,0 +1,34 @@
+console.log('JS E STARTS EXECUTION');
+var int;
+var p;
+var r;
+var t;
+var amt;
+
+function caluInterest(){
+
+p=document.getElementById("txt1").valueAsNumber;
+
+r=document.getElementById("txt2").valueAsNumber;
+
+t=document.getElementById("txt3").valueAsNumber;
+
+int=p*t*r/100;
+
+document.getElementById('result').value=int;
+
+console.log("interest is: "+int);
+}
+
+function caluAmount(){
+
+   // p=document.getElementById("principle").value;
+
+    amt=int+p;
+
+    document.getElementById('result').value=amt;
+
+ console.log("amount is: "+amt);
+}
+console.log('JS E STOPS EXECUTION');
+
